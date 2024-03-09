cacf7e0548a9de64d9ea8fc1bef4a1926856c54a
nagesha technology <asbconstruction19@gmail.com>
2024-03-09 13:11:58 +0530

    Printing colours

---
+++ 01-printing.sh
@@ -0,0 +1,9 @@
+#colour
+
+echo -e "\e[31mhellow in red colour\e[0m"
+echo -e "\e[32mhellow in green colour\e[0m"
+echo -e "\e[33mhellow in yello colour\e[0m"
+echo -e "\e[34mhellow in blue colour\e[0m"
+echo -e "\e[35mhellow in magenta colour\e[0m"
+echo -e "\e[36mhellow in cyan colour\e[0m"
+
